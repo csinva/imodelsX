cfff2de8c628618a1705f9d8f75ec50ee92a8e63
Chandan Singh <chandan_singh@berkeley.edu>
2023-01-31 16:44:35 -0800

    update load_hf_dataset

--- imodelsx/data.py
+++ imodelsx/data.py
@@ -3,8 +3,24 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 
-def load_huggingface_dataset(dataset_name, subsample_frac=1.0):
-    """Load text dataset from huggingface (with train/validation spltis) + return the relevant dataset key
+
+def load_huggingface_dataset(
+    dataset_name: str, subsample_frac: float = 1.0,
+    binary_classification: bool = False
+):
+    """Load text dataset from huggingface (with train/validation splits) + return the relevant dataset key
+    Params
+    ------
+    binary_classification: bool
+        Whether to convert a multiclass task into a binary one
+        Unless this function is modified, will take the class number with the lowest to indexes
+
+    Some examples       |    n_train     |    n_classes
+    rotten_tomatoes     |    ~9k         |    2
+    sst2                |    ~68k        |    2
+    tweet_eval          |    ~10k        |    2
+    financial_phrasebank|    ~2.3k       |    3
+    emotion             |    ~18k        |    6
     """
     # load dset
     if dataset_name == 'tweet_eval':
@@ -30,13 +46,40 @@ def load_huggingface_dataset(dataset_name, subsample_frac=1.0):
         del dset['unsupervised']
         dset['validation'] = dset['test']
 
-    # subsample datak
+    # subsample data
     if subsample_frac > 0:
         n = len(dset['train'])
         dset['train'] = dset['train'].select(np.random.choice(
             range(n), replace=False,
             size=int(n * subsample_frac)
         ))
+
+    # convert to binary classifications
+    if binary_classification:
+        if dataset_name == 'financial_phrasebank':
+            labels_to_keep_remap = {
+                0: 1, # negative
+                2: 0, # positive
+            }
+        elif dataset_name == 'emotion':
+            labels_to_keep_remap = {
+                0: 0, # sadness
+                1: 1, # joy
+            }
+        else:
+            labels_to_keep_keys = np.sort(np.unique(dset['train']['label']))[:2]
+            labels_to_keep_remap = {
+                labels_to_keep_keys[i]: i for i in range(2)
+            }
+        
+        # filter dset labels to only keep these labels
+        dset['train'] = dset['train'].filter(lambda ex: ex["label"] in labels_to_keep_remap)
+        dset['validation'] = dset['validation'].filter(lambda ex: ex["label"] in labels_to_keep_remap)
+
+        # map these labels to 0/1
+        dset['train'] = dset['train'].map(lambda ex: {'label': labels_to_keep_remap[ex['label']]})
+        dset['validation'] = dset['validation'].map(lambda ex: {'label': labels_to_keep_remap[ex['label']]})
+    
     return dset, dataset_key_text
 
 
@@ -48,3 +91,13 @@ def convert_text_data_to_counts_array(dset, dataset_key_text):
     y_test = dset['validation']['label']
     feature_names = v.get_feature_names_out().tolist()
     return X_train, X_test, y_train, y_test, feature_names
+
+if __name__ == '__main__':
+    dset, k = load_huggingface_dataset('emotion', 1, binary_classification=False)
+    print(dset)
+    print(dset['train'])
+    print(np.unique(dset['train']['label']))
+
+    dset, k = load_huggingface_dataset('emotion', 1, binary_classification=True)
+    print(dset)
+    print(np.unique(dset['train']['label']))
