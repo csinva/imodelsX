af5a8b94f4683f3b4f03244c851e1721e1b66c18
Chandan Singh <chandan_singh@berkeley.edu>
2023-02-12 16:34:24 -0800

    add regr metrics

--- imodelsx/metrics.py
+++ imodelsx/metrics.py
@@ -1,4 +1,8 @@
 import numpy as np
+from sklearn.metrics import accuracy_score, mean_squared_error, r2_score, roc_auc_score, \
+    precision_score, recall_score, balanced_accuracy_score, \
+        brier_score_loss
+from functools import partial
 
 def gini_score(y_true, y_pred):
     """Purer (more accurate) is better
@@ -19,3 +23,18 @@ def gini_binary(y_mean: float) -> float:
 
 def entropy_binary(y_mean: float) -> float:
     return -y_mean * np.log2(y_mean) - (1 - y_mean) * np.log2(1 - y_mean)
+
+metrics_classification_discrete = {
+    'accuracy': accuracy_score,
+    'precision': partial(precision_score, zero_division=0),
+    'recall': partial(recall_score, zero_division=0),
+    'balanced_accuracy': balanced_accuracy_score,
+}
+metrics_classification_proba = {
+    'roc_auc': roc_auc_score,
+    'brier_score_loss': brier_score_loss,
+}
+metrics_regression = {
+    'r2': r2_score,
+    'mse': mean_squared_error,
+}
