cefd961f4d0789311f49e290233792a5b54a977d
Chandan Singh <chandan_singh@berkeley.edu>
2023-03-01 20:51:10 -0800

    directly import LinearFinetune

--- imodelsx/__init__.py
+++ imodelsx/__init__.py
@@ -3,6 +3,7 @@
 """
 
 from .embgam.embgam import EmbGAMClassifier, EmbGAMRegressor
+from .linear_finetune import LinearFinetuneClassifier, LinearFinetuneRegressor
 from .d3.d3 import explain_datasets_d3
 from .iprompt.api import explain_dataset_iprompt
 from .iprompt.data import get_add_two_numbers_dataset
